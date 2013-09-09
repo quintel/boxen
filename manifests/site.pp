a7b88a0479cad9a18d8f95e586ccc193dc2036f0
Andre Medeiros <me@andremedeiros.info>
2013-09-09 15:35:09 +0200

    Get rid of unnecessary ruby & node versions.

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,14 +64,9 @@ node default {
   }
 
   # node versions
-  include nodejs::v0_4
-  include nodejs::v0_6
-  include nodejs::v0_8
   include nodejs::v0_10
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
   include ruby::1_9_3
   include ruby::2_0_0
 

3c75a5075d1e28da14fe35ba46bfda4a34150064
André Medeiros <me@andremedeiros.info>
2013-11-05 16:41:25 +0100

    Update etengine.pp

--- modules/projects/manifests/etengine.pp
+++ modules/projects/manifests/etengine.pp
@@ -2,7 +2,7 @@ class projects::etengine {
   boxen::project { 'etengine':
     mysql   => true,
     ruby    => '1.9.3',
-    source  => 'quintel/etengine'
+    source  => 'https://github.com/quintel/etengine.git'
   }
 }
 

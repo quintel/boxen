f63fbf1f7b8f024035077b0b4afb5fc9d0ce9e5e
André Medeiros <me@andremedeiros.info>
2013-11-05 16:41:54 +0100

    Update etmodel.pp

--- modules/projects/manifests/etmodel.pp
+++ modules/projects/manifests/etmodel.pp
@@ -2,7 +2,7 @@ class projects::etmodel {
   boxen::project { 'etmodel':
     mysql   => true,
     ruby    => '1.9.3',
-    source  => 'quintel/etmodel'
+    source  => 'https://github.com/quintel/etmodel.git'
   }
 }
 

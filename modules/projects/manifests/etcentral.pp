ab87b6d1ab1a73bbe763eb3458200229e8bf41a4
André Medeiros <me@andremedeiros.info>
2013-11-05 16:41:10 +0100

    Update etcentral.pp

--- modules/projects/manifests/etcentral.pp
+++ modules/projects/manifests/etcentral.pp
@@ -1,7 +1,7 @@
 class projects::etcentral {
   boxen::project { 'etcentral':
     ruby    => '1.9.3',
-    source  => 'quintel/etcentral'
+    source  => 'https://github.com/quintel/etcentral.git'
   }
 }
 
